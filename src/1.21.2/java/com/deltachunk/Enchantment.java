3ce37476bc42cb9ecdd2a30999f17cc1073d3250
user19870 <87924678+user19870@users.noreply.github.com>
2026-09-08 00:03:40 +0800

    Add files via upload

--- src/1.21.2/java/com/deltachunk/Enchantment.java
+++ src/1.21.2/java/com/deltachunk/Enchantment.java
@@ -49,6 +49,23 @@ public final class Enchantment {
                             "delta_delete"
                     )
             );
+//1.21.2/3新增方法
+private static ItemStack createEnchantedBook(Holder<Enchantment> enchantment, int level) {
+    ItemStack book = new ItemStack(Items.ENCHANTED_BOOK);
+
+    ItemEnchantments.Mutable enchantments =
+            new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
+    enchantments.set(enchantment, level);
+
+    book.set(
+            DataComponents.STORED_ENCHANTMENTS,
+            enchantments.toImmutable()
+    );
+
+    return book;
+}
+
+
  //tab
         @SubscribeEvent
 public static void addCreative(BuildCreativeModeTabContentsEvent event) {
@@ -66,15 +83,8 @@ public static void addCreative(BuildCreativeModeTabContentsEvent event) {
     Holder<net.minecraft.world.item.enchantment.Enchantment> deltaDelete =
             enchantments.getOrThrow(DELTA_DELETE);
 
-    ItemStack deltaAddBook =
-            EnchantedBookItem.createForEnchantment(
-                    new EnchantmentInstance(deltaAdd, 1)
-            );
-
-    ItemStack deltaDeleteBook =
-            EnchantedBookItem.createForEnchantment(
-                    new EnchantmentInstance(deltaDelete, 1)
-            );
+   ItemStack deltaAddBook = createEnchantedBook(deltaAdd, 1);
+ItemStack deltaDeleteBook = createEnchantedBook(deltaDelete, 1);
 
     event.accept(deltaAddBook);
     event.accept(deltaDeleteBook);
